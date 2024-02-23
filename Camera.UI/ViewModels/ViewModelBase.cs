a0192211ab4f32c81515dd4de779e58d6269598c
iiaypma228 <loolpip586@gmail.com>
2024-02-23 22:18:24 +0200

    Avalonia

---
+++ Camera.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Camera.UI.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Camera.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Camera.UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
